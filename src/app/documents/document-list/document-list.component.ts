95af4de9722901d1bbdb87cbb4d01421a07c526d
Aston Cook <coo19025@byui.edu>
2022-05-10 12:33:39 -0500

    week 4 assignment

--- src/app/documents/document-list/document-list.component.ts
+++ src/app/documents/document-list/document-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Document } from '../document.model'
 
 @Component({
   selector: 'cms-document-list',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./document-list.component.css']
 })
 export class DocumentListComponent implements OnInit {
+  @Output() selectedDocumentEvent = new EventEmitter<Document>();
+
+  documents: Document[] = [
+    new Document('1', 'CIT 260 - Object Oriented Programing', 'Object Oriented Programing', 'https://byui.instructure.com/', null!),
+    new Document('2', 'CIT 366 - Web Stack Development', 'Web Stack Development', 'https://byui.instructure.com/', null!),
+    new Document('3', 'CIT 425 - Warehousing', 'Warehousing', 'https://byui.instructure.com/', null!),
+    new Document('4', 'CIT 460 - Enterprise Development', 'Enterprise Development', 'https://byui.instructure.com/', null!),
+    new Document('5', 'CIT 495 - Senior Project', 'Senior Project', 'https://byui.instructure.com/', null!)
+  ]
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSelectedDocument(document: Document) {
+    this.selectedDocumentEvent.emit(document);
   }
 
 }
